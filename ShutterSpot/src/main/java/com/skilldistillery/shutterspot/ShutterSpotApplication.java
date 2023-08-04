6c428ddccb04163f16762173d3a6a6db5c687e6e
Dean Coffman <deansdetail@gmail.com>
2023-08-04 19:59:22 -0400

    create DB and get STS stubbed out and testing first entity green

---
+++ ShutterSpot/src/main/java/com/skilldistillery/shutterspot/ShutterSpotApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.shutterspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutterSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShutterSpotApplication.class, args);
+	}
+
+}
---
+++ ShutterSpot/src/test/java/com/skilldistillery/shutterspot/ShutterSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.shutterspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShutterSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
